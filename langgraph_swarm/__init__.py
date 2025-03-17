5c72508d276c92f263c5c6e5554198ab8c0dbc32
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2025-03-17 13:41:31 -0700

    Export SwarmState from __init__.py

--- langgraph_swarm/__init__.py
+++ langgraph_swarm/__init__.py
@@ -1,4 +1,4 @@
 from langgraph_swarm.handoff import create_handoff_tool
-from langgraph_swarm.swarm import add_active_agent_router, create_swarm
+from langgraph_swarm.swarm import SwarmState, add_active_agent_router, create_swarm
 
-__all__ = ["create_swarm", "add_active_agent_router", "create_handoff_tool"]
+__all__ = ["create_swarm", "add_active_agent_router", "create_handoff_tool", "SwarmState"]
