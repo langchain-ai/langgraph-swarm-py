04cb946961a32560bf99bc3150c08dcfdad2e0a4
vbarda <vadym@langchain.dev>
2025-04-29 11:15:34 -0400

    docs: improve docstring for create_swarm

--- langgraph_swarm/swarm.py
+++ langgraph_swarm/swarm.py
@@ -96,6 +96,9 @@ def create_swarm(
 
     Args:
         agents: List of agents to add to the swarm
+            An agent can be a LangGraph [CompiledStateGraph](https://langchain-ai.github.io/langgraph/reference/graphs/#langgraph.graph.state.CompiledStateGraph),
+            a functional API [workflow](https://langchain-ai.github.io/langgraph/reference/func/#langgraph.func.entrypoint),
+            or any other [Pregel](https://langchain-ai.github.io/langgraph/reference/pregel/#langgraph.pregel.Pregel) object.
         default_active_agent: Name of the agent to route to by default (if no agents are currently active).
         state_schema: State schema to use for the multi-agent graph.
         config_schema: An optional schema for configuration.
@@ -103,6 +106,49 @@ def create_swarm(
 
     Returns:
         A multi-agent swarm StateGraph.
+
+    Example:
+
+    ```python
+    from langgraph.checkpoint.memory import InMemorySaver
+    from langgraph.prebuilt import create_react_agent
+    from langgraph_swarm import create_handoff_tool, create_swarm
+
+    def add(a: int, b: int) -> int:
+        '''Add two numbers'''
+        return a + b
+
+    alice = create_react_agent(
+        "openai:gpt-4o",
+        [add, create_handoff_tool(agent_name="Bob")],
+        prompt="You are Alice, an addition expert.",
+        name="Alice",
+    )
+
+    bob = create_react_agent(
+        "openai:gpt-4o",
+        [create_handoff_tool(agent_name="Alice", description="Transfer to Alice, she can help with math")],
+        prompt="You are Bob, you speak like a pirate.",
+        name="Bob",
+    )
+
+    checkpointer = InMemorySaver()
+    workflow = create_swarm(
+        [alice, bob],
+        default_active_agent="Alice"
+    )
+    app = workflow.compile(checkpointer=checkpointer)
+
+    config = {"configurable": {"thread_id": "1"}}
+    turn_1 = app.invoke(
+        {"messages": [{"role": "user", "content": "i'd like to speak to Bob"}]},
+        config,
+    )
+    turn_2 = app.invoke(
+        {"messages": [{"role": "user", "content": "what's 5 + 7?"}]},
+        config,
+    )
+    ```
     """
     active_agent_annotation = state_schema.__annotations__.get("active_agent")
     if active_agent_annotation is None:
